80e1c723a7df9306722eb9d641ae4fc0c7782e6b
penelopeundercover <mjseiwert@outlook.com>
2022-10-09 19:44:25 -0500

    help

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,9 +2,85 @@ const teamArray = require("../index.js");
 const fs = require("fs");
 const path = require("path");
 
+function generateManager(managerData) {
+  return `<div class="col col-lg-3 m-2 mr-2">
+  <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <h2 class="card-title">${managerData.name}</h2>
+            <h5 class="card-title">"Manager"<h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${managerData.id}</li>
+            <li class="list-group-item">Email: <a href="mailto: ${managerData.email}">${managerData.email}</a></p></li></li>
+            <li class="list-group-item">${managerData.officeNumber}</li>
+          </ul>
+          
+        </div>
+        </div>
+  `;
+}
+
+function generateIntern(internData) {
+  return `<div class="col col-lg-3 m-2 mr-2">
+  <div class="card" style="width: 18rem">
+          <div class="card-body">
+             <h2 class="card-title">${internData.name}</h2>
+            <h5 class="card-title">"Intern"<h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${internData.id}</li>
+            <li class="list-group-item">Email: <a href="mailto: ${internData.email}">${internData.email}</a></p></li></li>
+            <li class="list-group-item">${internData.school}</li>
+          </ul>
+         
+        </div>
+        </div>
+  `;
+}
+
+function generateEngineer(engineerData) {
+  return `
+  <div class="col col-lg-3 m-2 mr-2">
+  <div class="card" style="width: 18rem">
+          <div class="card-body">
+                    <h2 class="card-title">${engineerData.name}</h2>
+            <h5 class="card-title">"Engineer"<h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${engineerData.id}</li>
+            <li class="list-group-item">Email: <a href="mailto: ${engineerData.email}">${engineerData.email}</a></p></li>
+            <li class="list-group-item">GitHub Username: <a href="https://www.github.com/${engineerData.github}" download target="_blank">${engineerData.github}</a></p></li>
+          </ul>
+          
+        </div>
+        </div>
+  `;
+}
+
+function addEmployees(teamData) {
+  const employeeHTML = [];
+  for (var x = 0; x < teamData.length; x++) {
+    const empRole = teamData[x].getRole();
+    switch (empRole) {
+      case "Manager":
+        employeeHTML.push(generateManager(teamData[x]));
+        break;
+      case "Intern":
+        employeeHTML.push(generateIntern(teamData[x]));
+        break;
+      case "Engineer":
+        employeeHTML.push(generateEngineer(teamData[x]));
+        break;
+      default:
+        return "Employee is not a manager, intern, or engineer.";
+    }
+  }
+  return employeeHTML.join("");
+}
+
 function generateHTML(teamArray) {
   const html = () => {
-    return `< !DOCTYPE html >
+    return `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -34,57 +110,8 @@ function generateHTML(teamArray) {
     <main>
       <div class="card-container">
         <div class="row">
-            <div class="col col-lg-3 m-2 mr-2">
-        <div class="card-1" style="width: 18rem">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h2 class="card-title">${teamArray[0].name}</h2>
-            <h5 class="card-title">"Manager"<h5>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">${teamArray[0].id}</li>
-            <li class="list-group-item">Email: <a href="mailto: ${teamArray[0].email}">${teamArray[0].email}</a></p></li></li>
-            <li class="list-group-item">${teamArray[0].officeNumber}</li>
-          </ul>
-          
+        ${addEmployees(teamArray)}
         </div>
-        </div>
-
-        <div class="col col-lg-3 m-2 mr-2">
-        <div class="card-2" style="width: 18rem">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-             <h2 class="card-title">${teamArray[1].name}</h2>
-            <h5 class="card-title">"Intern"<h5>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">${teamArray[1].id}</li>
-            <li class="list-group-item">Email: <a href="mailto: ${teamArray[1].email}">${teamArray[1].email}</a></p></li></li>
-            <li class="list-group-item">${teamArray[1].school}</li>
-          </ul>
-         
-        </div>
-        </div>
-
-<div class="col col-lg-3 m-2 mr-2">
-        <div class="card-3" style="width: 18rem">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-                    <h2 class="card-title">${teamArray[2].name}</h2>
-            <h5 class="card-title">"Engineer"<h5>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">${teamArray[2].id}</li>
-            <li class="list-group-item">Email: <a href="mailto: ${teamArray[2].email}">${teamArray[2].email}</a></p></li>
-            <li class="list-group-item">GitHub Username: <a href="https://www.github.com/${teamArray[2].githubusername}" download target="_blank">${teamArray[2].githubusername}</a></p></li>
-          </ul>
-          
-        </div>
-        </div>
-        </div>
-      </div>
-
-        
       </div>
     </main>
   </body>
